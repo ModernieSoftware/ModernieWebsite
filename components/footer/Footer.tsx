3d60b43c81d26d19fc7fe817bf6e5efebddd31e2
Heshan Navindu <kawhnkarachchi@std.appsc.sab.ac.lk>
2025-06-18 12:07:02 +0530

    footer content

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-import { Twitter, Linkedin, Github } from "lucide-react";
+import { Twitter, Linkedin, Github, Facebook } from "lucide-react";
 import Link from "next/link";
 import { Input } from "../ui/input";
 
@@ -15,17 +15,17 @@ const Footer = () => {
             <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
             <ul className="space-y-2">
               <li><Link href="/" className="text-gray-400 hover:text-white">Home</Link></li>
-              <li><Link href="/about" className="text-gray-400 hover:text-white">About</Link></li>
-              <li><Link href="/features" className="text-gray-400 hover:text-white">Features</Link></li>
-              <li><Link href="/contact" className="text-gray-400 hover:text-white">Contact</Link></li>
+              <li><Link href="/" className="text-gray-400 hover:text-white">About</Link></li>
+        
+              <li><Link href="/#product" className="text-gray-400 hover:text-white">Product</Link></li>
             </ul>
           </div>
           <div>
             <h3 className="text-lg font-semibold mb-4">Resources</h3>
             <ul className="space-y-2">
-              <li><Link href="/docs" className="text-gray-400 hover:text-white">Documentation</Link></li>
-              <li><Link href="/api" className="text-gray-400 hover:text-white">API Reference</Link></li>
-              <li><Link href="/blog" className="text-gray-400 hover:text-white">Blog</Link></li>
+              <li><Link href="/careers" className="text-gray-400 hover:text-white">Careers</Link></li>
+        
+              <li><Link href="/" className="text-gray-400 hover:text-white">Blog</Link></li>
             </ul>
           </div>
           <div>
@@ -39,13 +39,13 @@ const Footer = () => {
             <h3 className="text-lg font-semibold mb-4">Newsletter</h3>
                      <div className="flex space-x-4 mt-6">
               <a href="#" className="text-gray-400 hover:text-white">
-                <Twitter className="h-5 w-5" />
+                <Facebook className="h-5 w-5" />
               </a>
               <a href="#" className="text-gray-400 hover:text-white">
                 <Linkedin className="h-5 w-5" />
               </a>
               <a href="#" className="text-gray-400 hover:text-white">
-                <Github className="h-5 w-5" />
+                <Twitter className="h-5 w-5" />
               </a>
             </div>
           </div>
