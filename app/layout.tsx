36f3b5bd3dabb061350dcb5976e84ff24a576e03
Heshan Navindu <kawhnkarachchi@std.appsc.sab.ac.lk>
2025-07-03 14:09:07 +0530

    faviconn

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,7 +89,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon.png" />
-    
+         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
       </head>
